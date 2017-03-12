17a6485903c305d6c5f5c5b65ee2a0b33f4c2475
islam-Ellithy <islam.emmo@gmail.com>
2017-03-12 15:44:13 -0700

    1D closest pair

--- src/ClosestPair.java
+++ src/ClosestPair.java
@@ -1,45 +1,50 @@
 import java.awt.Point;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.Vector;
 
 public class ClosestPair {
-
-	Point[] points;
-	Point pairs[];
-
-	private double cloestPair(Point[] pX, Point[] pY, int n) {
+	Point[] points ;
+	Point[] pairs ;
+	double minD ;
+	private double cloestPair1D(Point[] pX, int n) {
 
 		if (n == 1) {
 			return Double.MAX_VALUE;
 		}
 		if (n == 2) {
-			pairs[0] = pY[0];
-			pairs[1] = pY[1];
-			return dist(pairs[0], pairs[1]);
+
+			if (dist(pX[0], pX[1]) < minD) {
+				pairs[0] = pX[0];
+				pairs[1] = pX[1];
+				minD = dist(pX[0], pX[1]);
+			}
+			return minD;
 		}
 
 		int mid = n / 2; // this is a vertical line
 		Point midPoint = pX[mid];
 
-		Point[] Pyl = new Point[mid + 1]; // y sorted points on left of vertical
-											// line
-		Point[] Pyr = new Point[n - mid - 1]; // y sorted points on right of
-												// vertical line
 		Point[] Pxl = new Point[mid + 1]; // x sorted points on left of vertical
 											// line
 		Point[] Pxr = new Point[n - mid - 1]; // x sorted points on right of
 												// vertical line
+
 		int li = 0, ri = 0; // indexes of left and right subarrays
 		for (int i = 0; i < n; i++) {
-			if (pY[i].x <= midPoint.x && li < Pyl.length) {
-				Pyl[li] = Pxl[li] = pY[i];
+			if (pX[i].x <= midPoint.x && li < Pxl.length) {
+				Pxl[li] = pX[i];
 				li++;
 			} else {
-				Pyr[ri] = Pxr[ri] = pY[i];
+				Pxr[ri] = pX[i];
 				ri++;
 			}
 		}
@@ -48,10 +53,10 @@ public class ClosestPair {
 		sortByXCoordinates(Pxl);
 		sortByXCoordinates(Pxr);
 
-		double dl = cloestPair(Pxl, Pyl, mid + 1);
-		double dr = cloestPair(Pxr, Pyr, n - mid - 1);
+		double dl = cloestPair1D(Pxl, mid + 1);
+		double dr = cloestPair1D(Pxr, n - mid - 1);
 
-		double minD = dl < dr ? dl : dr;
+		minD = dl < dr ? dl : dr;
 
 		// 5- create stip[] array that contain points close to minD
 		// - Create a vector and add to it only the points in the strip. Sort it
@@ -60,17 +65,14 @@ public class ClosestPair {
 		Point[] strip = new Point[n];
 		int count = 0;
 		for (int i = 0; i < n; i++) {
-			if (Math.abs(pY[i].x - midPoint.x) < minD) {
-				strip[count] = pY[i];
+			if (Math.abs(pX[i].x - midPoint.x) < minD) {
+				strip[count] = pX[i];
 				count++;
 			}
 		}
-
 		// 6- compute the distance of cloest pair inside the strip and
 		// return it if it is < minD
-
 		minD = cloestStripPair(strip, count, minD);
-
 		return minD;
 	}
 
@@ -83,90 +85,97 @@ public class ClosestPair {
 		});
 	}
 
-	public static void sortByYCoordinates(Point[] pointsByY) {
-
-		Arrays.sort(pointsByY, new Comparator<Point>() {
-			public int compare(Point a, Point b) {
-				return Integer.compare(a.y, b.y);
-			}
-		});
-	}
-
 	private double cloestStripPair(Point[] strip, int size, double minD) {
-		for (int i = 0; i < size - 1; i++) {
-			for (int j = i + 1; (j < size) && strip[j] != null && (strip[j].y - strip[i].y < minD); j++) {
+		int i, j;
+		for (i = 0; i < size - 1; i++) {// greatest size is 2
+			j = i+1;
+			if (strip[j].y - strip[i].y < minD)
 				if (dist(strip[j], strip[i]) < minD) {
 					minD = dist(strip[j], strip[i]);
 					pairs[0] = strip[j];
 					pairs[1] = strip[i];
+					System.out.println("Inside Strip\n" + pairs[0].toString() + "\n" + pairs[1].toString());
 				}
-			}
 		}
 		return minD;
 	}
 
-	public ClosestPair() {
+	public ClosestPair(Point[] points) {
 		pairs = new Point[2];
-	}
+		this.points = points;
+		sortByXCoordinates(this.points);
 
-	private  double dist(Point p1, Point p2) {
-		if((p1.y - p2.y)!=0 && (p1.x - p2.x)!=0)
-			return Math.abs((p1.x - p2.x))* Math.sqrt(1+Math.pow((double)(p1.y - p2.y)/(double)(p1.x - p2.x),2));
-		 return Math.sqrt(Math.pow(p1.y-p2.y, 2)+Math.pow(p1.x-p2.x, 2));
+		pairs[0] = this.points[0];
+		pairs[1] = this.points[1];
+
+		System.out.println(pairs[0].toString()+"\t"+pairs[1].toString());
+		minD = dist(pairs[0], pairs[1]);
+		this.cloestPair1D(points, points.length);
 	}
-/*
+	
+	/*
+	 * private double dist(Point p1, Point p2) { if ((p1.y - p2.y) != 0 && (p1.x
+	 * - p2.x) != 0) return Math.abs((p1.x - p2.x)) Math.sqrt(1 +
+	 * Math.pow((double) (p1.y - p2.y) / (double) (p1.x - p2.x), 2)); return
+	 * Math.sqrt(Math.pow(p1.y - p2.y, 2) + Math.pow(p1.x - p2.x, 2)); }
+	 */
+
 	public double dist(Point p, Point q) {
-		return Math.sqrt((double) (p.x - q.x) * (p.x - q.x) + (p.y - q.y) * (p.y - q.y));
-	}
-*/
-	public void print() {
-		System.out.println("two pairs are \n" + pairs[0].getX() +" - "+ pairs[0].getY() + 
-				"\n" + pairs[1].getX() +" - "+ pairs[1].getY()+"\n");
+		return Math.sqrt((double) Math.abs((p.x - q.x) * (p.x - q.x)));
 	}
 
-	public int Read(String path) {
-		Scanner in;
-		int sz = -1;
+	public void print() {
+		System.out.println("two pairs are \n" + pairs[0].getX() + " - " + pairs[0].getY() + "\n" + pairs[1].getX()
+				+ " - " + pairs[1].getY() + "\n" + this.minD);
+	}
+
+	public static void Write(String path, int len) {
+		Random randomGen = new Random();
+		FileWriter file;
 
 		try {
-			in = new Scanner(new File(path));
+			file = new FileWriter(path);
+			BufferedWriter bw = new BufferedWriter(file);
+			bw.write(len + "\n");
+			for (int i = 0; i < len; i++) {
+				bw.write(randomGen.nextInt(len) + "\n");
+			}
 
+			bw.close();
+			file.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Point[] Read(String path) {
+		Scanner in;
+		int sz = -1;
+		Point[] points = null;
+		try {
+			in = new Scanner(new File(path));
 			sz = in.nextInt();
 			points = new Point[sz];
 			int i = 0;
-			int x, y;
+			int x ;
 			while (in.hasNextInt()) {
 				x = in.nextInt();
-				y = in.nextInt();
-				points[i++] = new Point(x, y);
+				points[i++] = new Point(x, 0);
 			}
-
 			in.close();
 		} catch (FileNotFoundException e) {
-			// TODO: handle exception
 			e.printStackTrace();
 		}
-
-		return sz;
+		return points;
 	}
 
 	public static void main(String[] args) {
-
-		for (int j = 1; j <= 4; j++) {
-
-			ClosestPair cp = new ClosestPair();
-			int n = cp.Read("input" + j + ".txt");
-			Point[] pX = new Point[n];
-			Point[] pY = new Point[n];
-			for (int i = 0; i < n; i++) {
-				pX[i] = pY[i] = cp.points[i];
-			}
-			sortByXCoordinates(pX);
-			sortByYCoordinates(pY);
-			double distance = cp.cloestPair(pX, pY, n);
+		// ClosestPair.Write("input" + 2 + ".txt", 10);
+		for (int j = 2; j <= 2; j++) {
+			Point[] pX = ClosestPair.Read("input" + j + ".txt");
+			ClosestPair cp = new ClosestPair(pX);
 			cp.print();
-			System.out.println(distance);
 		}
 	}
-
 }
